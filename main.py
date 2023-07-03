a46762e17af56512ba19e77f475dc48ffe1a6e7d
Damiano D'Antuono <damianodantuono95@gmail.com>
2023-07-03 20:05:17 +0000

    ADDED PRINT LOG

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ def get_net_salary(request):
 
 
 def send_tgram_message(message):
+    TBOT_TOKEN = os.getenv('TBOT_TOKEN')
+    CHAT_ID = os.getenv('CHAT_ID')
+    print("TBOT TOKEN: ", TBOT_TOKEN)
+    print("CHAT_ID: ", CHAT_ID)
     url = f"https://api.telegram.org/{os.getenv('TBOT_TOKEN')}/sendMessage"
     r = requests.get(url, {'chat_id': os.getenv('CHAT_ID'), 'text': message})
-    if r.status_code == 200:
-        return
-    raise ValueError(f"Error sending message: {message}")
