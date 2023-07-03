45e6f4a7b9e9fe06a81803e6c76f17271bedece8
Damiano D'Antuono <damianodantuono95@gmail.com>
2023-07-03 19:36:02 +0000

    return tgram bot

--- main.py
+++ main.py
@@ -3,12 +3,4 @@ import functions_framework
 @functions_framework.http
 def get_net_salary(request):
     request_json = request.get_json(silent=True)
-    request_args = request.args
-
-    if request_json and 'name' in request_json:
-        name = request_json['name']
-    elif request_args and 'name' in request_args:
-        name = request_args['name']
-    else:
-        name = 'World'
-    return 'Hello {}!'.format(name)
+    return request_json
